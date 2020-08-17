9f1f23a768bd6835ea4e6fb5b14de969d6c20cc4
Hugo Arnoult <hugo.arnoult83@gmail.com>
2020-08-17 17:26:13 +0200

    add validates user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+  has_many :philosophers
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
 end

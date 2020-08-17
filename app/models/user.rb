b626d92c4a9e71c75036413f08681d77d5df9530
Martin Fesneau <martin.fesneau@gmail.com>
2020-08-17 17:47:45 +0200

    added associations to philosopher & user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-  has_many :philosophers
+  has_many :reservations
+  has_many :philosophers, through :reservations
   
   validates :first_name, presence: true
   validates :last_name, presence: true

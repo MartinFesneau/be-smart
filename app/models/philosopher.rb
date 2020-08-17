8dda2d9e37ecf998e816797096a23883a6c28ba9
Corentin Malvaux <corentin.malvaux@gmail.com>
2020-08-17 16:00:06 +0200

    added validations to philosopher model

--- app/models/philosopher.rb
+++ app/models/philosopher.rb
@@ -1,2 +1,4 @@
 class Philosopher < ApplicationRecord
+  belongs_to :user
+  validates :nationality, :last_name, :birthday, :specialty, :available_location, :prestations, :price_per_night, :description, presence: true
 end

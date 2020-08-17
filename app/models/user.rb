e471119074b4170d6e482b37bafa036aa761cf97
Martin Fesneau <martin.fesneau@gmail.com>
2020-08-17 18:00:33 +0200

    removed through reservations from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
   has_many :reservations
-  has_many :philosophers, through :reservations
+  has_many :philosophers
   
   validates :first_name, presence: true
   validates :last_name, presence: true

1fa852e2068fe17c42526c76fbc93645b29209b4
Hugo Arnoult <hugo.arnoult83@gmail.com>
2020-08-17 18:38:44 +0200

    Setup cloudinary and add relations in models

--- app/models/philosopher.rb
+++ app/models/philosopher.rb
@@ -1,6 +1,8 @@
 class Philosopher < ApplicationRecord
+  belongs_to :user
   has_many :reservations
   has_many :users, through :reservations
-  belongs_to :user
+  has_many_attached :photos
+  
   validates :nationality, :last_name, :birthday, :specialty, :available_location, :prestations, :price_per_night, :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable
   has_many :reservations
   has_many :philosophers
+  has_one_attached :photo
   
   validates :first_name, presence: true
   validates :last_name, presence: true

e60ee7bd66afef466266be01fea5a46d52df4f79
Corentin Malvaux <corentin.malvaux@gmail.com>
2020-08-18 11:27:33 +0200

    Added user references to philosopher

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_163350) do
+ActiveRecord::Schema.define(version: 2020_08_18_091558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2020_08_17_163350) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_philosophers_on_user_id"
   end
 
   create_table "reservations", force: :cascade do |t|
@@ -81,6 +83,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_163350) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "philosophers", "users"
   add_foreign_key "reservations", "philosophers"
   add_foreign_key "reservations", "users"
 end

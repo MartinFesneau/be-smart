03d414452ba1e6b16a2861dc1847d1071f6b5c0c
Hugo Lavorel <hugo.lavorel@gmail.com>
2020-08-18 10:39:07 +0200

    Create seed for user and philosophers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Philosopher.destroy_all
+User.destroy_all
+
+puts 'Creating user'
+
+jean = User.create(
+            email:"jean.martin@gmail.com",
+            first_name:"Jean",
+            last_name:"Martin",
+            description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, commodi.",
+            phone_number:"01 98 29 22 76",
+            password: "123456789"
+            )
+
+jean.save!
+
+puts 'End creating user'
+
+puts 'Creating philosopher'
+
+10.times do
+  philosophe = Philosopher.create(nationality: "Greek",
+                      first_name:"",
+                      last_name: Faker::GreekPhilosophers.name,
+                      birthday: Faker::Date.between(from: '0001-01-01', to: '1000-01-01'),
+                      specialty: Faker::Educator.subject,
+                      available_location: ["Athens", "Heraklion", "Rhodes"].sample,
+                      price_per_night: (25..75).to_a.sample,
+                      description: Faker::GreekPhilosophers.quote,
+                      prestations: "Peux parler des heures" )
+  philosophe.user = jean
+  philosophe.save!
+end
+
+puts 'End creating philosopher'

06123fffee28127075960db6038f66ef2f8ce7f5
Corentin Malvaux <corentin.malvaux@gmail.com>
2020-08-18 13:10:59 +0200

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_091558) do
+ActiveRecord::Schema.define(version: 2020_08_18_103220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_091558) do
     t.date "birthday"
     t.string "specialty"
     t.string "available_location"
-    t.string "prestations"
+    t.string "prestations", default: [], array: true
     t.integer "price_per_night"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false

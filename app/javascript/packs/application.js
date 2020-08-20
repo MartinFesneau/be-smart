1f302661ce7959503b9a9f68de6f92288fec409a
Hugo Arnoult <hugo.arnoult83@gmail.com>
2020-08-20 11:26:56 +0200

    install algolia places

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,9 +28,11 @@ import "bootstrap";
 // Internal imports, e.g:
 import { initMapbox } from "../plugins/init_mapbox" ;
 import { formActive } from '../components/form_active';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   initMapbox();
+  initAutocomplete();
   formActive();
 });

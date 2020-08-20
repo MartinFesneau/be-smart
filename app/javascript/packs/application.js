c9d7ea58989bf4cc76987039feda698b3ebd6cb0
Hugo Lavorel <hugo.lavorel@gmail.com>
2020-08-20 18:02:18 +0200

    Dimitri a fait des choses

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,7 +34,7 @@ import "../plugins/flatpickr"
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  initMapbox();
   initAutocomplete();
   formActive();
+  initMapbox();
 });

3bf075f3068ce7c2ea05ebfaf9ae3747973820e8
Hugo Lavorel <hugo.lavorel@gmail.com>
2020-08-21 16:17:12 +0200

    dynamic price repaired

--- app/javascript/components/dynamic_price.js
+++ app/javascript/components/dynamic_price.js
@@ -8,16 +8,23 @@ const dynamicPrice = () => {
 
   if (startDate) {
     startDate.addEventListener("change", (event) => {
-      const price = parseInt( priceElement.innerText, 10 )
-      const start = new Date( startDate.value.split(' ')[0]).setHours(0,0,0,0);
-      const end = new Date( endDate.value ).setHours(0,0,0,0);
-      // console.log(end);
+      const price = parseInt(priceElement.innerText, 10)
+      const startDateArray = startDate.value.split(' ')[0].split('.').map(number => parseInt(number, 10));
+      // const endDateArray = endDate.value.split('.').map(number => parseInt(number, 10));
+      const start = new Date(startDateArray);
+      // const start = new Date(startDate.value.split(' ')[0]).setHours(0,0,0,0);
+      const end = new Date(endDate.value);
+      console.log(start)
+      console.log(end)
       // console.log(start);
-      
-      const nights = ( end - start )/1000/60/60/24;
+      // console.log(end);
+      // console.log(end - start);
+      // console.log(Date.parse(end) - Date.parse(start));
+
+      const nights = (end - start)/1000/60/60/24;
       const totalPrice = price * nights;
-      console.log(totalPrice);
-      
+      // console.log(totalPrice);
+
       if (end) {
         priceForm.classList.remove("d-none");
         priceForm.innerText = "";

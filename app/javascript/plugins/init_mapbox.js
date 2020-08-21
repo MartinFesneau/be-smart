b685dd4d8f8195eb0e110f7ee2b4ae071811969b
Hugo Lavorel <hugo.lavorel@gmail.com>
2020-08-21 11:28:39 +0200

    Refacto mapbox js

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,34 +1,41 @@
+import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from 'mapbox-gl';
+
+const mapElement = document.getElementById('map');
+
+const buildMap = () => {
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  return new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+  });
+}
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+    new mapboxgl.Marker({color: 'hsl(349, 100%, 61%)'})
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
+      .addTo(map);
+  });
+}
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   console.log(markers);
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 0, maxZoom: 13, duration: 0 });
 };
 
+
 const initMapbox = () => {
-  const mapElement = document.getElementById('map');
-
-
   if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
-
+    const map = buildMap();
     const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-      new mapboxgl.Marker({color: 'hsl(349, 100%, 61%)'})
-        .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup)
-        .addTo(map);
-    });
-
+    addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
-
+    console.log(map.getCenter());
   }
 };
 
